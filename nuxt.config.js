76042a8a276e307b4110115f9e5b38f95d6bbccf
skeptycal <skeptycal@gmail.com>
2019-12-02 22:02:28 -0600

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,57 +1,56 @@
 export default {
-  mode: 'universal',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: 'Skeptycal',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      },
-      {
-        property: 'og:title',
-        content: 'Test title',
-        template: chunk => `${chunk} - My page`, //or as string template: '%s - My page',
-        vmid: 'og:title'
-      }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap'
-      }
-    ]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [],
-  /*
-   ** Build configuration
-   */
-  build: {
+    mode: 'universal',
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
+    head: {
+        title: 'Skeptycal',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            },
+            {
+                property: 'og:title',
+                content: 'Test title',
+                template: chunk => `${chunk} - My page`, //or as string template: '%s - My page',
+                vmid: 'og:title'
+            }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap'
+            }
+        ]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 }
